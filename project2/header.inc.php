2b410634db2de1d24cc1a71b8af04a62dba00506
Sameerxh <32047689+Sameerxh@users.noreply.github.com>
2026-03-18 07:00:09 +0000

    cretaed header with navigation link

--- project2/header.inc.php
+++ project2/header.inc.php
@@ -1,14 +1,12 @@
-<header class="header">  
-  <div class="header__wrapper">
-    <h1 class="header__heading">Art Store</h1>
-    <nav class="navigation">
-      <ul class="navigation__list">
-        <?php
-        /* write loop to generate list items as follows: 
-            <li class='navigation__list-item'><a href='index.php'>Home</a></li> */
-
-        ?>
-      </ul>
-    </nav>  
-  </div>
-</header> 
+     
+     <?php include 'data.inc.php'; ?>
+        <header>
+          <h1>Art Store</h1>
+          <nav>
+            <ul>
+              <?php foreach ($links as $link) { ?>
+                <li><a href="<?php echo $link['url']; ?>"><?php echo $link['label']; ?></a></li>
+                <?php } ?>
+            </ul>
+          </nav>
+        </header>
